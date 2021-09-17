90aea64cfd32dfc9082b3cb81f9a4244a2ec000f
abf149 <afeld2012@gmail.com>
2021-09-17 16:29:20 +0000

    matraptor script

---
+++ exercises/2020.ispass/matraptor.sh
@@ -0,0 +1,5 @@
+timeloop-mapper arch/matraptor_like-int16.yaml \
+                arch/components/*.yaml \
+                prob/prob.yaml \
+                mapper/mapper.yaml \
+                constraints/*.yaml
