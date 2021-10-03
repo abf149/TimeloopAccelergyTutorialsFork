cafaa410122c764424d3ce3124a8d147e67bf691
Andrew Feldman <afeld2012@gmail.com>
2021-10-03 15:23:48 -0400

    WIP

--- exercises/2020.ispass/matraptor/matraptor.sh
+++
@@ -1,5 +0,0 @@
-timeloop-mapper arch/matraptor_like-int16.yaml \
-                arch/components/*.yaml \
-                prob/prob.yaml \
-                mapper/mapper.yaml \
-                constraints/*.yaml
