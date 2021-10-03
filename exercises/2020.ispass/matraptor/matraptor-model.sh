25b29ce42740cd6c824982b7839ed11448a60ef4
abf149 <afeld2012@gmail.com>
2021-10-03 09:56:16 +0000

    SOW

---
+++ exercises/2020.ispass/matraptor/matraptor-model.sh
@@ -0,0 +1,9 @@
+#timeloop-mapper arch/matraptor_like-int16.yaml \
+#                arch/components/*.yaml \
+#                prob/prob.yaml \
+#                mapper/mapper.yaml \
+#                constraints/*.yaml
+
+
+timeloop-model arch/matraptor_like-int16.yaml arch/components/*.yaml \
+               prob/* map/*
