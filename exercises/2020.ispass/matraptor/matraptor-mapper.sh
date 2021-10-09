7680636c597aceb7c281c091997c1f4eea5a0dcd
abf149 <afeld2012@gmail.com>
2021-10-09 02:49:25 +0000

    matraptor-like

--- exercises/2020.ispass/matraptor/matraptor-mapper.sh
+++
@@ -1,9 +0,0 @@
-timeloop-mapper arch/matraptor_like-int16.yaml \
-                arch/components/*.yaml \
-                prob/prob.yaml \
-                mapper/mapper.yaml \
-                constraints/*.yaml
-
-
-# timeloop-model arch/matraptor_like-int16.yaml \
-#                arch/components/*.
