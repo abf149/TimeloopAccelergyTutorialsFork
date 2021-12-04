02567d7bc4c5cdbcc807666b12bb3413b274f43e
Andrew Feldman <afeld2012@gmail.com>
2021-12-04 02:11:43 +0000

    experiment 1 dataflow comparison results

---
+++ exercises/2020.ispass/sparse_dataflow_experiments/experiment_1/sparse-exploration-mapper-outer-product.sh
@@ -0,0 +1,11 @@
+#timeloop-mapper arch/matraptor_like-int16.yaml \
+#                arch/components/*.yaml \
+#                prob/prob.yaml \
+#                mapper/mapper.yaml \
+#                constraints/*.yaml
+
+
+timeloop-mapper arch/sparse-exporation-architecture.yaml arch/components/*.yaml prob/prob.yaml mapper/mapper.yaml constraints/outer-product-arch-constraints.yaml sparse-opt/naive-row-wise-product.yaml  -o output/
+
+#arch/components/*.yaml \
+#                map/*
